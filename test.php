375d0ca2886127e675478612bde81e0b3e2fdbf6
lohith564 <lohith564@gmail.com>
2017-05-05 20:21:13 +0530

    Add files via upload

---
+++ test.php
@@ -0,0 +1,17 @@
+name,year,cast,language,rating,type,box office,producers,
+
+CREATE TABLE details(
+name varchar(100),
+year int(10),
+cast text,
+language varchar(10),
+rating float,
+type varchar(20),
+boxoffice varchar(25),
+producers varchar(25),
+description text
+);
+
+$name="Sarainodu";
+y
+INSERT into details Values('Kodi',2016,'Dhanush','Tamil',7.3,'Thriller','₹500 million','R.S Durai Senthilkumar','Kodi is a 2016 Indian Tamil-language political thriller film written and directed by R. S. Durai Senthilkumar and produced by Vetrimaaran under his banner Grass Root Film Company.');
